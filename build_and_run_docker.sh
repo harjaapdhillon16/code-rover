01c476f24be61a50704bfe63f1f031a3d5fa78a7
Harjaap Dhillon <harvydhillon16@gmail.com>
2024-06-05 05:26:39 +0530

    changes

--- build_and_run_docker.sh
+++ build_and_run_docker.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# Clone the repository containing the Dockerfile and dependencies
+git clone https://github.com/nus-apr/auto-code-rover tmp_dockerfiles
+
+# Navigate into the cloned directory
+cd tmp_dockerfiles
+
 # Build the Docker image
-docker build -f Dockerfile -t acr .
+docker build -f Dockerfile -t my_custom_image .
+
+# Clean up: Remove the cloned directory
+cd ..
+rm -rf tmp_dockerfiles
 
 # Run the Docker container
-docker run -it -e OPENAI_KEY="${OPENAI_KEY:-OPENAI_API_KEY}" -p 3000:3000 -p 5000:5000 acr
+docker run -it -e OPENAI_KEY="${OPENAI_KEY:-OPENAI_API_KEY}" -p 3000:3000 -p 5000:5000 my_custom_image
